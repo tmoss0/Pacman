a4723b70fa7e2887917e13ddbc9c24e149923d47
Tim Moss <tmoss726@hotmail.com>
2015-01-16 23:10:52 -0500

    Initial Upload
    Initial pacman upload

---
+++ Assets/Scripts/Pacdot.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Pacdot : MonoBehaviour 
+{
+	void OnTriggerEnter2D(Collider2D co)
+	{
+		if (co.gameObject.name == "pacman") 
+		{
+			Destroy(gameObject);
+		}
+	}
+}
